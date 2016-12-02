6ec37ccd1664395caf54583006bab702941c591f
Daniel Standage <daniel.standage@gmail.com>
2016-12-02 10:28:45 -0800

    Added fasta, including regression test for parse_seq_dict bug

---
+++ tests/test_fasta.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#
+# -----------------------------------------------------------------------------
+# Copyright (c) 2016 The Regents of the University of California
+#
+# This file is part of kevlar (http://github.com/standage/kevlar) and is
+# licensed under the MIT license: see LICENSE.
+# -----------------------------------------------------------------------------
+
+import pytest
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+import kevlar
+
+
+@pytest.fixture
+def bogusseqs():
+    seq = '>seq1\nACGT\n>seq2 yo\nGATTACA\nGATTACA\n>seq3\tdescrip\nATGATGTGA'
+    return seq.split('\n')
+
+
+def test_parse(bogusseqs):
+    seqs = { defline: seq for defline, seq in kevlar.fasta.parse(bogusseqs) }
+    assert seqs == {
+        '>seq1': 'ACGT',
+        '>seq2 yo': 'GATTACAGATTACA',
+        '>seq3\tdescrip': 'ATGATGTGA',
+    }
+
+
+def test_seq_dict(bogusseqs):
+    d = kevlar.fasta.parse_seq_dict(bogusseqs)
+    assert d == {
+        'seq1': 'ACGT',
+        'seq2': 'GATTACAGATTACA',
+        'seq3': 'ATGATGTGA',
+    }
