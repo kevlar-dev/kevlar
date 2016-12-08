c165491ef1f4122b6a9e154e594d2d4af4cd922c
Daniel Standage <daniel.standage@gmail.com>
2016-12-07 16:08:06 -0800

    Provenance of one of the test .bam files

--- tests/data/make-bogus-genome.sh
+++ tests/data/make-bogus-genome.sh
@@ -3,28 +3,37 @@ set -eo pipefail
 
 # Check software prereqs
 which make-random-genome.py
+which bwa
+which samtools
+
+mkdir -p bogus-genome
 
 # Simulate genome
 make-random-genome.py --length 15000 --seed 42 --name bogus-genome-chr1 \
-    > bogus-genome-chr1.fa
+    > bogus-genome/chr1.fa
 make-random-genome.py --length 10000 --seed 2468 --name bogus-genome-chr2 \
-    > bogus-genome-chr2.fa
+    > bogus-genome/chr2.fa
 make-random-genome.py --length 12000 --seed 13579 --name bogus-genome-chr3 \
-    > bogus-genome-chr3.fa
+    > bogus-genome/chr3.fa
 make-random-genome.py --length 500 --seed 112358 --name bogus-genome-contam \
-    > bogus-genome-contam1.fa
+    > bogus-genome/contam1.fa
 
-cat bogus-genome-chr?.fa > bogus-genome-refr.fa
-cat bogus-genome-chr2.fa bogus-genome-chr3.fa > bogus-genome-mask-chr1.fa
-cat bogus-genome-chr1.fa bogus-genome-chr3.fa > bogus-genome-mask-chr2.fa
-cat bogus-genome-chr?.fa bogus-genome-contam1.fa > bogus-genome-refr-contam.fa
-./mutate1.py bogus-genome-refr.fa > bogus-genome-seq-pool-1snp.fa
-./mutate3.py bogus-genome-refr.fa > bogus-genome-seq-pool-3snps.fa
-./mutate1.py bogus-genome-refr-contam.fa > bogus-genome-seq-pool-1snp-contam.fa
+cat bogus-genome/chr?.fa > bogus-genome/refr.fa
+cat bogus-genome/chr2.fa bogus-genome/chr3.fa > bogus-genome/mask-chr1.fa
+cat bogus-genome/chr1.fa bogus-genome/chr3.fa > bogus-genome/mask-chr2.fa
+cat bogus-genome/chr?.fa bogus-genome/contam1.fa > bogus-genome/refr-contam.fa
+./mutate1.py bogus-genome/refr.fa > bogus-genome/seq-pool-1snp.fa
+./mutate3.py bogus-genome/refr.fa > bogus-genome/seq-pool-3snps.fa
+./mutate1.py bogus-genome/refr-contam.fa > bogus-genome/seq-pool-1snp-contam.fa
 
-./make-bogus-reads.py bogus-genome-refr-contam.fa > bogus-reads-small.fq
+./make-bogus-reads.py bogus-genome/refr-contam.fa > bogus-genome/reads-small.fq
 
-rm -f bogus-genome-chr?.fa bogus-genome-contam1.fa
+cd bogus-genome/
+bwa index refr.fa refr.fa
+bwa mem -k 13 -a refr.fa reads-small.fq \
+    | samtools view -b -o - \
+    | samtools sort -o reads.bam -
+rm refr.fa.*
+cd ../
 
-mkdir -p bogus-genome/
-mv bogus-genome-* bogus-reads-small.fq bogus-genome/
+rm -f bogus-genome/chr?.fa bogus-genome/contam1.fa
