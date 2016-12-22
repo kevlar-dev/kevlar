e31a21baea044e9139651ea5cd215bd122b1281a
Daniel Standage <daniel.standage@gmail.com>
2016-12-22 14:02:13 -0800

    File encoding issues

--- tests/test_find.py
+++ tests/test_find.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
 # -----------------------------------------------------------------------------
 # Copyright (c) 2016 The Regents of the University of California
