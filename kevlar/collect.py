d4d107cd738d4c302e3568b46ca301fdf3d0491e
Daniel Standage <daniel.standage@gmail.com>
2017-01-06 11:25:41 -0800

    Refactor the "collect" script

--- kevlar/collect.py
+++ kevlar/collect.py
@@ -9,6 +9,7 @@
 
 from __future__ import print_function
 import argparse
+import re
 import sys
 
 import khmer
@@ -32,11 +33,10 @@ def subparser(subparsers):
                            'files from the "kevlar find" command')
 
 
-def load_input(infile, nodegraph, unique_kmers):
+def load_input(infile, nodegraph, variants):
     readid = None
     reads_consumed = 0
     kmers_consumed = 0
-    kmer_instances = 0
     for line in infile:
         if line.startswith('@'):
             # Load the 4 lines of the Fastq record
@@ -45,56 +45,54 @@ def load_input(infile, nodegraph, unique_kmers):
             _ = next(infile)
             _ = next(infile)
 
-            _, nkmers = nodegraph.consume(seq)
+            nkmers = nodegraph.consume(seq)
             reads_consumed += 1
             kmers_consumed += nkmers
         elif line.endswith('#\n'):
-            novel_kmer = re.search('^ *(\S+)', line)
-            kmer_instances += 1
-            minkmer = kevlar.revcommin(novel_kmer)
-            unique_kmers.add(minkmer)
-            kmer_reads[minkmer].append(readid)
+            novel_kmer = re.search('^ *(\S+)', line).group(1)
+            variants.add_kmer(novel_kmer, readid)
 
-    return kmers_consumed, reads_consumed, kmer_instances, unique_kmers
+    return reads_consumed, kmers_consumed
+
+
+def load_all_inputs(filelist, nodegraph, variants, logfile=sys.stderr):
+    kmers_consumed = 0
+    reads_consumed = 0
+    for infile in filelist:
+        print('[kevlar::collect] Loading', infile, file=logfile)
+        with open(infile, 'r') as fh:
+            nr, nk = load_input(fh, nodegraph, variants)
+            reads_consumed += nr
+            kmers_consumed += nk
+    message = '    {:d} reads'.format(reads_consumed)
+    message += ' and {:d} k-mers consumed'.format(kmers_consumed)
+    message += '; {:d} instances'.format(variants.nkmerinst)
+    message += ' of {:d} unique novel k-mers'.format(variants.nkmers)
+    print(message, file=logfile)
+
+
+def assemble_contigs(nodegraph, variants, collapse=True, logfile=sys.stderr):
+    print('[kevlar::collect] Retrieving linear paths', file=logfile)
+    for kmer in variants.kmers:
+        contig = nodegraph.assemble_linear_path(kmer)
+        if contig == '':
+            print('    WARNING: no linear path found for k-mer', kmer,
+                  file=args.logfile)
+            continue
+        variants.add_contig(contig, kmer)
+    print('    {:d} linear paths'.format(variants.ncontigs), file=logfile)
+
+    if collapse:
+        print('[kevlar::collect] Collapsing linear paths', file=logfile)
+        variants.collapse()
+        print('    {:d} collapsed linear paths'.format(variants.npaths),
+              file=logfile)
 
 
 def main(args):
     nodegraph = khmer.Nodegraph(args.ksize, args.memory / 4, 4)
-
-    kmers_consumed = 0
-    reads_consumed = 0
-    novel_kmer_instances = 0
-    unique_novel_kmers = set()
-    kmer_reads = defaultdict(list)
-    for infile in args.find_output:
-        print('[kevlar::collect] Loading', infile, file=args.logfile)
-        with open(infile, 'r') as fh:
-            data = load_input(fh, nodegraph, unique_novel_kmers, kmer_reads)
-            kmers_consumed += data[0]
-            reads_consumed += data[1]
-            novel_kmer_instances += data[2]
-    message = '    {:d} reads'.format(reads_consumed)
-    message += ' and {:d} k-mers consumed'.format(kmers_consumed)
-    message += '; {:d} instances'.format(novel_kmer_instances)
-    message += ' of {:d} unique novel k-mers'.format(len(unique_novel_kmers))
-    print(message, file=args.logfile)
-
-    print('[kevlar::collect] Retrieving linear paths', file=args.logfile)
     variants = kevlar.VariantSet()
-    for kmer in unique_novel_kmers:
-        linear_path = nodegraph.assemble_linear_path(kmer)
-        if linear_path == '':
-            print('    WARNING: no linear path found for k-mer', kmer,
-                  file=args.logfile)
-            continue
-        for readid in kmer_reads[kmer]:
-            variants.add_kmer(kmer, readid, linear_path)
-    print('    {:d} linear paths'.format(variants.npaths), file=args.logfile)
 
-    if args.collapse:
-        print('[kevlar::collect] Collapsing linear paths', file=args.logfile)
-        variants.collapse()
-        print('    {:d} collapsed linear paths'.format(variants.npaths),
-              file=args.logfile)
-
-    variants.path_table(outstream=args.out)
+    load_all_inputs(args.find_output, nodegraph, variants, args.logfile)
+    assemble_contigs(nodegraph, variants, args.collapse, args.logfile)
+    variants.write(outstream=args.out)
