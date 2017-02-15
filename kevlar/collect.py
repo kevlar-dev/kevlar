4b37e6c66500c070d6bce0d30c4bdaa0b734c737
Daniel Standage <daniel.standage@gmail.com>
2017-02-14 21:28:45 -0800

    Two passes for filtering inflated abundances

--- kevlar/collect.py
+++ kevlar/collect.py
@@ -25,9 +25,14 @@ def subparser(subparsers):
                            type=khmer_args.memory_setting, metavar='MEM',
                            help='total memory to allocate for the node '
                            'graph; default is 1M')
-    subparser.add_argument('--max-fpr', type=float, default=0.2, metavar='FPR',
+    subparser.add_argument('--minabund', type=int, default=5, metavar='Y',
+                           help='minimum case abundance required to call a '
+                           'k-mer novel; used to filter out k-mers with '
+                           'inflated abundances; should equal the value of '
+                           '--case_min used in "kevlar find"; default is 5')
+    subparser.add_argument('--max-fpr', type=float, default=0.1, metavar='FPR',
                            help='terminate if the expected false positive rate'
-                           ' is higher than the specified FPR; default is 0.2')
+                           ' is higher than the specified FPR; default is 0.1')
     subparser.add_argument('-k', '--ksize', type=int, default=31, metavar='K',
                            help='k-mer size; default is 31')
     subparser.add_argument('--ignore', metavar='KMER', nargs='+',
@@ -37,59 +42,74 @@ def subparser(subparsers):
                            help='output file; default is terminal (stdout)')
     subparser.add_argument('--collapse', action='store_true', help='collapse '
                            'linear paths contained in other linear paths')
-    subparser.add_argument('find_output', nargs='+',
-                           type=argparse.FileType('r'), help='one or more '
-                           'output files from the "kevlar find" command')
+    subparser.add_argument('find_output', nargs='+', help='one or more output '
+                           'files from the "kevlar find" command')
 
 
-def load_input(infile, nodegraph, variants):
-    readid = None
-    reads_consumed = 0
-    kmers_consumed = 0
-    for line in infile:
-        if line.startswith('@'):
-            # Load the 4 lines of the Fastq record
-            readid = line[1:].strip()
-            seq = next(infile).strip()
-            _ = next(infile)
-            _ = next(infile)
-
-            nkmers = nodegraph.consume(seq)
-            reads_consumed += 1
-            kmers_consumed += nkmers
-        elif line.endswith('#\n'):
-            novel_kmer = re.search('^ *(\S+)', line).group(1)
-            variants.add_kmer(novel_kmer, readid)
-
-    return reads_consumed, kmers_consumed
-
-
-def load_all_inputs(filelist, nodegraph, variants, maxfpr=0.2,
+def load_all_inputs(filelist, countgraph, variants, minabund=5, maxfpr=0.2,
                     logfile=sys.stderr):
     kmers_consumed = 0
     reads_consumed = 0
-    for infile in filelist:
-        infilename = 'input file'
-        if hasattr(infile, "name"):
-            infilename = infile.name
-        print('[kevlar::collect] Loading', infilename, file=logfile)
-        nr, nk = load_input(infile, nodegraph, variants)
-        reads_consumed += nr
-        kmers_consumed += nk
-    fpr = kevlar.calc_fpr(nodegraph)
+    readids = dict()
+    print('[kevlar::collect]',
+          'Loading input, pass 1 (recalculate k-mer abundance)',
+          file=logfile)
+    for filename in filelist:
+        print('   ', filename, file=logfile)
+        with open(filename, 'r') as infile:
+            for line in infile:
+                if line.startswith('@'):
+                    # Load the 4 lines of the Fastq record
+                    readid = line[1:].strip()
+                    seq = next(infile).strip()
+                    _ = next(infile)
+                    _ = next(infile)
+
+                    if readid not in readids:
+                        readids[readid] = True
+                        kmers_consumed += countgraph.consume(seq)
+                        reads_consumed += 1
+
+                elif line.endswith('#\n'):
+                    # Ignore the novel k-mers in the first pass
+                    continue
+
+    del readids  # Free the memory!
+    fpr = kevlar.calc_fpr(countgraph)
     message = '    {:d} reads'.format(reads_consumed)
     message += ' and {:d} k-mers consumed'.format(kmers_consumed)
-    message += '; {:d} instances'.format(variants.nkmerinst)
-    message += ' of {:d} unique novel k-mers'.format(variants.nkmers)
     message += '; estimated false positive rate is {:1.3f}'.format(fpr)
     print(message, file=logfile)
     if fpr > maxfpr:
         sys.exit(1)
 
+    print('[kevlar::collect]',
+          'Loading input, pass 2 (store novel k-mers)',
+          file=logfile)
+    for filename in filelist:
+        print('   ', filename, file=logfile)
+        with open(filename, 'r') as infile:
+            for line in infile:
+                if line.startswith('@'):
+                    # Load the 4 lines of the Fastq record
+                    readid = line[1:].strip()
+                    seq = next(infile).strip()
+                    _ = next(infile)
+                    _ = next(infile)
+                elif line.endswith('#\n'):
+                    # Second pass, double check the abundance, and then add
+                    novel_kmer = str(re.search('^ *(\S+)', line).group(1))
+                    if countgraph.get(novel_kmer) >= minabund:
+                        variants.add_kmer(novel_kmer, readid)
 
-def assemble_contigs(nodegraph, variants, kmers_to_ignore=None,
+    message = '    found {:d} instances'.format(variants.nkmerinst)
+    message += ' of {:d} unique novel k-mers'.format(variants.nkmers)
+    print(message, file=logfile)
+
+
+def assemble_contigs(countgraph, variants, kmers_to_ignore=None,
                      collapse=True, debug=False, logfile=sys.stderr):
-    asm = khmer.JunctionCountAssembler(nodegraph)
+    asm = khmer.JunctionCountAssembler(countgraph)
     print('[kevlar::collect] Retrieving linear paths', file=logfile)
     for kmer in variants.kmers:
         if kmers_to_ignore and kmer in kmers_to_ignore:
@@ -113,11 +133,11 @@ def assemble_contigs(nodegraph, variants, kmers_to_ignore=None,
 
 
 def main(args):
-    nodegraph = khmer.Nodegraph(args.ksize, args.memory / 4, 4)
+    countgraph = khmer.Countgraph(args.ksize, args.memory / 4, 4)
     variants = kevlar.VariantSet()
 
-    load_all_inputs(args.find_output, nodegraph, variants, args.max_fpr,
+    load_all_inputs(args.find_output, countgraph, variants, args.max_fpr,
                     args.logfile)
-    assemble_contigs(nodegraph, variants, args.ignore, args.collapse,
+    assemble_contigs(countgraph, variants, args.ignore, args.collapse,
                      args.debug, args.logfile)
     variants.write(outstream=args.out)
--- tests/test_collect.py
+++ tests/test_collect.py
@@ -17,37 +17,37 @@ import khmer
 import kevlar
 
 
-@pytest.mark.parametrize('filename,creads,ckmers,ukmers,kmerinst', [
-    ('tests/data/trio1/novel_1_1,2.txt', 18, 684, 13, 171),
-    ('tests/data/trio1/novel_2_1,2.txt', 53, 2014, 39, 540),
-    ('tests/data/trio1/novel_3_1,2.txt', 178, 6764, 424, 5782),
-    ('tests/data/trio1/novel_4_1,2.txt', 16, 608, 11, 99),
-    ('tests/data/trio1/novel_5_3,4.txt', 16, 553, 9, 109),
-    ('tests/data/trio1/novel_6_5,6.txt', 9, 336, 2, 9),
-])
-def test_load_input(filename, creads, ckmers, ukmers, kmerinst):
-    ng = khmer.Nodegraph(13, 3e5 / 4, 4)
-    vs = kevlar.VariantSet()
-    with open(filename, 'r') as infile:
-        nreads, nkmers = kevlar.collect.load_input(infile, ng, vs)
-    assert nreads == creads
-    assert nkmers == ckmers
-    assert vs.nkmers == ukmers
-    assert vs.nkmerinst == kmerinst
+# @pytest.mark.parametrize('filename,creads,ckmers,ukmers,kmerinst', [
+#     ('tests/data/trio1/novel_1_1,2.txt', 18, 684, 13, 171),
+#     ('tests/data/trio1/novel_2_1,2.txt', 53, 2014, 39, 540),
+#     ('tests/data/trio1/novel_3_1,2.txt', 178, 6764, 424, 5782),
+#     ('tests/data/trio1/novel_4_1,2.txt', 16, 608, 11, 99),
+#     ('tests/data/trio1/novel_5_3,4.txt', 16, 553, 9, 109),
+#     ('tests/data/trio1/novel_6_5,6.txt', 9, 336, 2, 9),
+# ])
+# def test_load_input(filename, creads, ckmers, ukmers, kmerinst):
+#     ng = khmer.Nodegraph(13, 3e5 / 4, 4)
+#     vs = kevlar.VariantSet()
+#     with open(filename, 'r') as infile:
+#         nreads, nkmers = kevlar.collect.load_input(infile, ng, vs)
+#     assert nreads == creads
+#     assert nkmers == ckmers
+#     assert vs.nkmers == ukmers
+#     assert vs.nkmerinst == kmerinst
 
 
 def test_load_all_inputs():
-    ng = khmer.Nodegraph(13, 3e5 / 4, 4)
+    cg = khmer.Countgraph(13, 24e5 / 4, 4)
     vs = kevlar.VariantSet()
+    minabund = 8
     maxfpr = 0.2
     log = StringIO()
 
     batches = [3, 4, 5, 7, 8]
     filepattern = 'tests/data/trio1/novel_1_1,2_batch{:d}.txt'
     infilenames = [filepattern.format(batch) for batch in batches]
-    infiles = [open(fn, 'r') for fn in infilenames]
 
-    kevlar.collect.load_all_inputs(infiles, ng, vs, maxfpr, log)
+    kevlar.collect.load_all_inputs(infilenames, cg, vs, minabund, maxfpr, log)
     assert vs.nkmers == 6
     assert vs.nkmerinst == 49
     assert vs.nreads == 10
