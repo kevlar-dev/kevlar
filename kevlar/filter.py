bc37866ce2cb66d116179a62e9e7b38cc31861e8
Daniel Standage <daniel.standage@gmail.com>
2017-03-27 16:27:56 -0700

    Independent filter script

--- kevlar/filter.py
+++ kevlar/filter.py
@@ -17,14 +17,88 @@ from khmer import khmer_args
 import kevlar
 
 
+class ReadSet(object):
+
+    def __init__(self):
+        self._reads = dict()
+        self._kmers = dict()
+
+        self._masked_count = 0
+        self._masked_distinct = 0
+        self._lowabund_count = 0
+        self._lowabund_distinct = 0
+        self._valid_count = 0
+        self._valid_distinct = 0
+        self._novalidkmers_count = 0
+
+    def __len__(self):
+        assert len(self._reads) == len(self._kmers)
+        return len(self._reads)
+
+    @property
+    def interesting(self):
+        instances = sum([
+            self._masked_count,
+            self._lowabund_count,
+            self._valid_count
+        ])
+        distinct = sum([
+            self._masked_distinct,
+            self._lowabund_distinct,
+            self._valid_distinct
+        ])
+        return instances, distinct
+
+    def __iter__(self):
+        for readid in self._reads:
+            if len(self._kmers[readid]) == 0:
+                self._novalidkmers_count += 1
+            else:
+                yield self._reads[readid], self._kmers[readid]
+
+    def add(self, record, kmers):
+        if record.name in self._reads:
+            assert record.sequence == self._reads[record.name].sequence
+            self._kmers[record.name].update(kmers)
+        else:
+            self._reads[record.name] = record
+            self._kmers[record.name] = kmers
+
+    def validate(self, counts, refr=None, minabund=5):
+        masked = set()
+        lowabund = set()
+        valid = set()
+        for readid in self._kmers:
+            validated_kmers = dict()
+            for offset in self._kmers[readid]:
+                kmer, abundances = self._kmers[readid][offset]
+                if refr and refr.get(kmer) > 0:
+                    self._masked_count += 1
+                    if kmer not in masked:
+                        self._masked_distinct += 1
+                        masked.add(kmer)
+                elif counts.get(kmer) < minabund:
+                    self._lowabund_count += 1
+                    if kmer not in lowabund:
+                        self._lowabund_distinct += 1
+                        lowabund.add(kmer)
+                else:
+                    validated_kmers[offset] = (kmer, abundances)
+                    self._valid_count += 1
+                    if kmer not in valid:
+                        self._valid_distinct += 1
+                        valid.add(kmer)
+            self._kmers[readid] = validated_kmers
+
+
 def subparser(subparsers):
     subparser = subparsers.add_parser('filter', add_help=False)
     subparser._positionals.title = 'Required inputs'
 
     refr_args = subparser.add_argument_group(
         'Reference genome',
-        'A k-mer labeled as "interesting" by `kevlar find` should be discarded'
-        ' if it is present in the supplied reference genome.'
+        'A reference genome is not required, but if available can be supplied '
+        'to discard "interesting" k-mers that turn out not to be novel.'
     )
     refr_args.add_argument('--refr', metavar='FILE', type=str, default=None,
                            help='reference genome in Fasta/Fastq format')
@@ -40,10 +114,12 @@ def subparser(subparsers):
     filter_args = subparser.add_argument_group(
         'Filtering k-mers',
         'Memory constraints often require running `kevlar find` with false '
-        'positive rates (FPRs) in the 0.1 - 0.2 range. This will result in '
-        'some k-mers with highly inflated abundances. With much less input '
-        'data, this script can achieve a much lower FPR --> exact k-mer '
-        'abundances.'
+        'positive rates (FPRs) in the 0.1 - 0.2 range, resulting in some '
+        'k-mers reported with highly inflated abundances. This script handles '
+        'a much smaller amount of data, and in limited memory can achieve a '
+        'much lower FPR, compute exact k-mer abundances, and discard '
+        '"interesting" k-mers whose abundances were incorrectly reported '
+        'previously.'
     )
     filter_args.add_argument('--abund-memory', metavar='MEM', default='1e6',
                              type=khmer_args.memory_setting,
@@ -68,129 +144,126 @@ def subparser(subparsers):
     misc_args.add_argument('-k', '--ksize', type=int, default=31, metavar='K',
                            help='k-mer size; default is 31')
     misc_args.add_argument('-o', '--out', type=argparse.FileType('w'),
-                           metavar='OUT',
+                           metavar='FILE',
                            help='output file; default is terminal (stdout)')
+    misc_args.add_argument('--aug-out', type=argparse.FileType('w'),
+                           metavar='FILE',
+                           help='optional augmented Fastq output')
 
-    subparser.add_argument('find_output', nargs='+', help='one or more output '
-                           'files from the `kevlar find` command')
+    subparser.add_argument('augfastq', nargs='+', help='one or more files in '
+                           '"augmented" Fastq format (a la `kevlar find` '
+                           'output)')
 
 
-def load_refr(refrfile, ksize, memory, logfile=sys.stderr):
-    print('[kevlar::collect] Loading reference from ' + refrfile, file=logfile)
+def load_refr(refrfile, ksize, memory, maxfpr=0.001, logfile=sys.stderr):
     buckets = memory * khmer._buckets_per_byte['nodegraph'] / 4
     refr = khmer.Nodetable(ksize, int(buckets), 4)
     nr, nk = refr.consume_seqfile(refrfile)
-    message = '    {:d} reads and {:d} k-mers consumed'.format(nr, nk)
-    print(message, file=logfile)
-    return refr
-
-
-def recalc_abund(filelist, ksize, memory, maxfpr=0.1, logfile=sys.stderr):
-    countgraph = khmer.Countgraph(ksize, memory / 4, 4)
-    kmers_consumed = 0
-    reads_consumed = 0
-    readids = set()
-    print('[kevlar::collect]',
-          'Loading input, pass 1; recalculating k-mer abundances',
-          file=logfile)
-    for filename in filelist:
-        print('   ', filename, file=logfile)
-        with open(filename, 'r') as infile:
-            for line in infile:
-                if line.startswith('@'):
-                    # Load the 4 lines of the Fastq record
-                    readid = line[1:].strip()
-                    seq = next(infile).strip()
-                    _ = next(infile)
-                    _ = next(infile)
-
-                    if readid not in readids:
-                        readids.add(readid)
-                        kmers_consumed += countgraph.consume(seq)
-                        reads_consumed += 1
-
-                elif line.endswith('#\n'):
-                    # Ignore the novel k-mers in the first pass
-                    continue
-
-    fpr = kevlar.calc_fpr(countgraph)
-    message = '    {:d} reads'.format(reads_consumed)
-    message += ' and {:d} k-mers consumed'.format(kmers_consumed)
+    fpr = kevlar.calc_fpr(refr)
+    message = '    {:d} sequences and {:d} k-mers consumed'.format(nr, nk)
     message += '; estimated false positive rate is {:1.3f}'.format(fpr)
     print(message, file=logfile)
     if fpr > maxfpr:
+        print('[kevlar::filter] FPR too high, bailing out', file=logfile)
         sys.exit(1)
-    return countgraph
+    return refr
 
 
-def load_novel_kmers(filelist, countgraph, refr=None, minabund=5,
-                     logfile=sys.stderr):
-    variants = kevlar.VariantSet()
-    print('[kevlar::collect] Loading input, pass 2; store novel k-mers',
-          file=logfile)
-    masked = 0
-    discarded = 0
+def recalc_abund(filelist, ksize, memory, maxfpr=0.001, logfile=sys.stderr):
+    countgraph = khmer.Countgraph(ksize, memory / 4, 4)
+    read_inst_consumed = 0
+    int_kmer_parsed = 0
+    readset = ReadSet()
     for filename in filelist:
-        print('   ', filename, file=logfile)
+        print('    -', filename, file=logfile)
         with open(filename, 'r') as infile:
-            for line in infile:
-                if line.startswith('@'):
-                    # Load the 4 lines of the Fastq record
-                    readid = line[1:].strip()
-                    seq = next(infile).strip()
-                    _ = next(infile)
-                    _ = next(infile)
-                elif line.endswith('#\n'):
-                    # Second pass, double check the abundance, and then add
-                    novel_kmer = str(re.search('^ *(\S+)', line).group(1))
-                    if refr and refr.get(novel_kmer) > 0:
-                        masked += 1
-                    elif countgraph.get(novel_kmer) < minabund:
-                        discarded += 1
-                    else:
-                        variants.add_kmer(novel_kmer, readid)
+            for record, kmers in kevlar.parse_augmented_fastq(infile):
+                if record.name not in readset._reads:
+                    countgraph.consume(record.sequence)
+                readset.add(record, kmers)
+                read_inst_consumed += 1
 
-    message = '    found {:d} instances'.format(variants.nkmerinst)
-    message += ' of {:d} unique novel k-mers'.format(variants.nkmers)
-    message += '; {:d} k-mers masked by the reference genome'.format(masked)
-    message += '; {:d} k-mers with inflated counts discarded'.format(discarded)
+    fpr = kevlar.calc_fpr(countgraph)
+    message = '    {:d} instances'.format(read_inst_consumed)
+    message += ' of {:d} reads consumed'.format(len(readset))
+    message += '; estimated false positive rate is {:1.3f}'.format(fpr)
     print(message, file=logfile)
-    return variants
+    if fpr > maxfpr:
+        print('[kevlar::filter] FPR too high, bailing out', file=logfile)
+        sys.exit(1)
+    return readset, countgraph
 
 
-def assemble_contigs(countgraph, variants, kmers_to_ignore=None,
-                     collapse=True, debug=False, logfile=sys.stderr):
-    asm = khmer.JunctionCountAssembler(countgraph)
-    print('[kevlar::collect] Retrieving linear paths', file=logfile)
-    for kmer in variants.kmers:
-        if kmers_to_ignore and kmer in kmers_to_ignore:
-            continue
-        if debug:
-            print('[kevlar::collect]     DEBUG kmer:', kmer, file=logfile)
-        contigs = asm.assemble(kmer)
-        for i, contig in enumerate(contigs):
-            if contig == '':
-                print('    WARNING: no linear path found for k-mer', kmer,
-                      file=logfile)
-                continue
-            variants.add_contig(contig, kmer)
-    print('    {:d} linear paths'.format(variants.ncontigs), file=logfile)
+def validate_and_print(readset, countgraph, refr=None, minabund=5,
+                       outfile=sys.stdout, augout=None, logfile=sys.stderr):
+    readset.validate(countgraph, refr, minabund)
+    for record, kmers in readset:
+        khmer.utils.write_record(record, outfile)
+        if augout:
+            khmer.utils.write_record(record, augout)
+            for offset in sorted(kmers):
+                kmer, abundances = kmers[offset]
+                abundstr = ' '.join([str(a) for a in abundances])
+                print(' ' * offset, kmer, ' ' * 10, abundstr, '#', sep='',
+                      file=augout)
 
-    if collapse:
-        print('[kevlar::collect] Collapsing linear paths', file=logfile)
-        variants.collapse()
-        print('    {:d} collapsed linear paths'.format(variants.ncontigs),
-              file=logfile)
+    int_instances, int_distinct = readset.interesting
+    message = '    processed {:d} instances'.format(int_instances)
+    message += ' of {:d} distinct "interesting" k-mers'.format(int_distinct)
+    message += ' in {:d} reads'.format(len(readset))
+    message += '\n        '
+    message += '{:d} instances'.format(readset._masked_count)
+    message += ' of {:d} distinct k-mers'.format(readset._masked_distinct)
+    message += ' masked by the reference genome'
+    message += '\n        '
+    message += '{:d} instances'.format(readset._lowabund_count)
+    message += ' of {:d} distinct k-mers'.format(readset._lowabund_distinct)
+    message += ' discarded due to low abundance'
+    message += '\n        '
+    message += '{:d} instances'.format(readset._valid_count)
+    message += ' of {:d} distinct k-mers'.format(readset._valid_distinct)
+    message += ' validated as novel'
+    message += '\n        '
+    message += '{:d} reads'.format(readset._novalidkmers_count)
+    message += ' with no surviving valid k-mers ignored'
+    print(message, file=logfile)
 
 
 def main(args):
+    timer = kevlar.Timer()
+    timer.start()
+
     refr = None
-    if args.mask:
-        refr = load_refr(args.refr, args.ksize, args.refr_memory)
-    countgraph = recalc_abund(args.find_output, args.ksize, args.memory,
-                              args.max_fpr, args.logfile)
-    variants = load_novel_kmers(args.find_output, countgraph, refr,
-                                args.minabund, args.logfile)
-    assemble_contigs(countgraph, variants, args.ignore, args.collapse,
-                     args.debug, args.logfile)
-    variants.write(outstream=args.out)
+    if args.refr:
+        timer.start('loadrefr')
+        print('[kevlar::filter] Loading reference genome from',
+              args.refr, file=args.logfile)
+        refr = load_refr(args.refr, args.ksize, args.refr_memory,
+                         args.refr_max_fpr, args.logfile)
+        elapsed = timer.stop('loadrefr')
+        print('[kevlar::filter]',
+              'Reference genome loaded in {:.2f} sec'.format(elapsed),
+              file=args.logfile)
+
+    timer.start('recalc')
+    print('[kevlar::filter] Loading input; recalculate k-mer abundances',
+          'de-duplicate reads and merge k-mers',
+          file=args.logfile)
+    readset, countgraph = recalc_abund(args.augfastq, args.ksize,
+                                       args.abund_memory, args.abund_max_fpr,
+                                       args.logfile)
+    elapsed = timer.stop('recalc')
+    print('[kevlar::filter] Input loaded in {:.2f} sec'.format(elapsed))
+
+    timer.start('validate')
+    print('[kevlar::filter] Validate k-mers and print reads',
+          file=args.logfile)
+    validate_and_print(readset, countgraph, refr, args.min_abund, args.out,
+                       args.aug_out, args.logfile)
+    elapsed = timer.stop('validate')
+    print('[kevlar::filter] k-mers validated and reads printed',
+          'in {:.2f} sec'.format(elapsed), file=args.logfile)
+
+    total = timer.stop()
+    message = 'Total time: {:.2f} seconds'.format(total)
+    print('[kevlar::filter]', message, file=args.logfile)
---
+++ tests/test_filter.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+#
+# -----------------------------------------------------------------------------
+# Copyright (c) 2017 The Regents of the University of California
+#
+# This file is part of kevlar (http://github.com/standage/kevlar) and is
+# licensed under the MIT license: see LICENSE.
+# -----------------------------------------------------------------------------
+
+import glob
+import pytest
+import sys
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+import khmer
+import kevlar
+
+
+def test_load_refr():
+    refr = kevlar.collect.load_refr('tests/data/bogus-genome/refr.fa', 25, 1e7)
+    assert refr.get('GGCCCCGAACTAGGGGGCCTACGTT') > 0
+    assert refr.get('GCTGGCTAAATTTTCATACTAACTA') > 0
+    assert refr.get('G' * 25) == 0
+
+
+def test_recalc_abund_beta():
+    filelist = glob.glob('tests/data/collect.beta.?.txt')
+    countgraph = kevlar.filter.recalc_abund(filelist, 19, 1e3)
+
+    kmers = [
+        'AGGGGCGTGACTTAATAAG', 'GGGCGTGACTTAATAAGGT',
+        'TAGGGGCGTGACTTAATAA', 'GGGGCGTGACTTAATAAGG',
+    ]
+    for kmer in kmers:
+        assert countgraph.get(kmer) == 8
+
+
+def test_load_novel_kmers_alpha():
+    filelist = ['tests/data/collect.alpha.txt']
+    countgraph = kevlar.collect.recalc_abund(filelist, 19, 1e3)
+    vs = kevlar.collect.load_novel_kmers(filelist, countgraph)
+    assert vs.nkmers == 4
+    assert vs.nreads == 8
+
+    badkmers = ['CAGGCCAGGGATCGCCGTG']
+    goodkmers = [
+        'AGGGGCGTGACTTAATAAG', 'GGGCGTGACTTAATAAGGT',
+        'TAGGGGCGTGACTTAATAA', 'GGGGCGTGACTTAATAAGG',
+    ]
+    for kmer in badkmers:
+        assert kmer not in vs.kmers and kevlar.revcom(kmer) not in vs.kmers
+    for kmer in goodkmers:
+        assert kmer in vs.kmers or kevlar.revcom(kmer) in vs.kmers
+
+
+def test_load_novel_kmers_beta():
+    filelist = glob.glob('tests/data/collect.beta.?.txt')
+    countgraph = kevlar.collect.recalc_abund(filelist, 19, 1e3)
+    vs = kevlar.collect.load_novel_kmers(filelist, countgraph)
+    assert vs.nkmers == 4
+    assert vs.nreads == 8
+
+    countgraph = kevlar.collect.recalc_abund(filelist, 19, 1e3)
+    vs = kevlar.collect.load_novel_kmers(filelist, countgraph, minabund=9)
+    assert vs.nkmers == 0
+    assert vs.nreads == 0
+
+
+def test_load_novel_kmers_withrefr():
+    kmer = 'AGGGGCGTGACTTAATAAG'
+    filelist = glob.glob('tests/data/collect.beta.?.txt')
+    countgraph = kevlar.collect.recalc_abund(filelist, 19, 1e3)
+    refr = khmer.Nodetable(19, 1e3, 2)
+    refr.add(kmer)
+    vs = kevlar.collect.load_novel_kmers(filelist, countgraph, refr)
+    assert vs.nkmers == 3
+    assert kmer not in vs.kmers and kevlar.revcom(kmer) not in vs.kmers
