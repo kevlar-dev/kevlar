b5b31b1ce926ecd15e7f90d567ef3bb14aaae613
Daniel Standage <daniel.standage@gmail.com>
2017-03-27 11:48:25 -0700

    Better tests

--- tests/test_fasta.py
+++ tests/test_fasta.py
@@ -53,3 +53,42 @@ def test_aug_fastq_reader():
             kmer, abundances = kmers[offset]
             assert abundances == [8, 0, 0]
     assert n == 7
+
+
+def test_aug_fastq_reader_e1():
+    infile = open('tests/data/example1.augfastq', 'r')
+    record, kmers = next(kevlar.parse_augmented_fastq(infile))
+
+    assert record.name == 'e1'
+    assert record.sequence == (
+        'TTAACTCTAGATTAGGGGCGTGACTTAATAAGGTGTGGGCCTAAGCGTCT'
+    )
+    assert len(kmers) == 2
+
+    assert 13 in kmers
+    assert kmers[13][0] == 'AGGGGCGTGACTTAATAAG'
+    assert kmers[13][1] == [12, 15, 1, 1]
+
+    assert 15 in kmers
+    assert kmers[15][0] == 'GGGCGTGACTTAATAAGGT'
+    assert kmers[15][1] == [20, 28, 0, 1]
+
+
+def test_aug_fastq_reader_e2():
+    infile = open('tests/data/example2.augfastq', 'r')
+    record, kmers = next(kevlar.parse_augmented_fastq(infile))
+
+    assert record.name == 'ERR894724.125497791/1'
+    assert record.sequence == (
+        'TAGCCAGTTTGGGTAATTTTAATTGTAAAACTTTTTTTTCTTTTTTTTTGATTTTTTTTTTTCAAGCAG'
+        'AAGACGGCATACGAGCTCTTTTCACGTGACTGGAGTTCAGACGTGTGCTCTTCCGAT'
+    )
+    assert len(kmers) == 2
+
+    assert 74 in kmers
+    assert kmers[74][0] == 'GGCATACGAGCTCTTTTCACGTGACTGGAGT'
+    assert kmers[74][1] == [23, 0, 0]
+
+    assert 83 in kmers
+    assert kmers[83][0] == 'GCTCTTTTCACGTGACTGGAGTTCAGACGTG'
+    assert kmers[83][1] == [23, 0, 0]
