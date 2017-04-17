b8424c2e1f084721e4daf0c9433f26deb6cf488f
Daniel Standage <daniel.standage@gmail.com>
2017-04-17 13:53:14 -0700

    Fix unicode issue

--- kevlar/cli/__init__.py
+++ kevlar/cli/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
 # -----------------------------------------------------------------------------
 # Copyright (c) 2017 The Regents of the University of California
