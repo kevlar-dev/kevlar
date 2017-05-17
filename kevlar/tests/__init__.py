a3a541410dd235db58a7703043797c0e5701af57
Daniel Standage <daniel.standage@gmail.com>
2017-05-17 08:57:06 -0700

    Make test data file glob deterministic

--- kevlar/tests/__init__.py
+++ kevlar/tests/__init__.py
@@ -18,4 +18,4 @@ def data_file(basename):
 
 def data_glob(globstr):
     datadir = pkg_resources.resource_filename('kevlar', 'tests/data')
-    return glob.glob(datadir + '/' + globstr)
+    return sorted(glob.glob(datadir + '/' + globstr))
