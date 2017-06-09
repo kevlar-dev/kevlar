d7440e96f1fd02326d812b204117c6d43beeb059
Daniel Standage <daniel.standage@gmail.com>
2017-06-09 14:42:35 -0700

    New simulation

--- kevlar/overlap.py
+++ kevlar/overlap.py
@@ -219,7 +219,7 @@ def graph_init_strict(reads, kmers, minabund=5, maxabund=500, logstream=None):
     graph = networkx.Graph()
     nkmers = len(kmers)
     for n, minkmer in enumerate(kmers, 1):
-        if n % 100 == 0:  # pragma: no cover
+        if n % 100 == 0 and logstream:  # pragma: no cover
             msg = 'processed {:d}/{:d} shared novel k-mers'.format(n, nkmers)
             print('[kevlar::overlap]    ', msg, sep='', file=logstream)
 
