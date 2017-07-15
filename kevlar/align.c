462662ba4ddd00108b8f1cc724adf486ff21c67e
Daniel Standage <daniel.standage@gmail.com>
2017-07-15 13:34:06 -0700

    Need a working function before I write Python wrapper

--- kevlar/align.c
+++ kevlar/align.c
@@ -1,5 +1,6 @@
-#include <Python.h>
 #include "ksw2.h"
+#include <string>
+#include <string.h>
 
 // Stolen shamelessly from ksw2/cli.c
 unsigned char seq_nt4_table[256] = {
@@ -36,7 +37,7 @@ static void ksw_gen_simple_mat(int m, int8_t *mat, int8_t a, int8_t b)
         mat[(m - 1) * m + j] = 0;
 }
 
-static PyObject *align_call(PyObject *self, PyObject *args)
+std::string align_call(const char *target, const char *query)
 {
     /**
      * Align `query` to `target` using the `ksw_extz` algorithm. See
@@ -46,43 +47,39 @@ static PyObject *align_call(PyObject *self, PyObject *args)
      * @param query     contig assembled from variant-associated reads
      * @returns         CIGAR string of the alignment
      */
-    const char *target;
-    const char *query;
     char cigar[512];
     size_t ci = 0;
 
-    int8_t score_match = 1;
-    int8_t penalty_mismatch = 2;
-    int8_t penalty_gapopen = 5
-    int8_t penalty_gap_extend = 0.5;
-    int8_t alphabet_size = 5;
+    uint8_t score_match = 1;
+    uint8_t penalty_mismatch = 2;
+    uint8_t penalty_gapopen = 5;
+    uint8_t penalty_gap_extend = 1;
+    uint8_t alphabet_size = 5;
 
     int8_t matrix[25];
     ksw_extz_t ez;
-
-    if (!PyArg_ParseTuple(args, "ss", &target, &query)) {
-        return NULL;
-    }
 
     memset(&ez, 0, sizeof(ksw_extz_t));
     ksw_gen_simple_mat(5, matrix, score_match, -penalty_mismatch);
 
     ksw_extz(
         NULL, // memory pool
-        strlen(query), query, strlen(target), target, alphabet_size, matrix,
-        penalty_gapopen, penalty_gap_extend,
+        strlen(query), (const uint8_t *)query,
+        strlen(target), (const uint8_t *)target,
+        alphabet_size, matrix, penalty_gapopen, penalty_gap_extend,
         -1, // bandwidth
         -1, // zdrop
         0, //flag
-        ez
+        &ez
     );
 
     // Stolen shamelessly from ksw2/cli.c
-    for (int i = 0; i < ez->n_cigar; ++i) {
+    for (int i = 0; i < ez.n_cigar; ++i) {
         ci += sprintf(
-            cigar + ci, "%d%c", ez->cigar[i] >> 4,"MID"[ez->cigar[i]&0xf]
+            cigar + ci, "%d%c", ez.cigar[i] >> 4,"MID"[ez.cigar[i]&0xf]
         );
     }
 
-    return PyUnicodeFromString(cigar);
+    std::string cigarstring(cigar);
+    return cigarstring;
 }
