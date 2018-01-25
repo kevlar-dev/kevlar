3e9ac21961ef60d193f493ed34d1c88a38606591
Daniel Standage <daniel.standage@gmail.com>
2018-01-25 13:27:51 -0800

    Laying groundwork for filtering by number of interesting k-mers

--- kevlar/alac.py
+++ kevlar/alac.py
@@ -10,17 +10,30 @@
 import sys
 import kevlar
 from kevlar.assemble import assemble_greedy, assemble_fml_asm
+from kevlar.augment import augment
 from kevlar.localize import localize
 from kevlar.call import call
 
 
+def augment_and_mark(augseqs, nakedseqs):
+    for seq in augment(augseqs, nakedseqs):
+        seq.name = '{:s} nikmers={:d}'.format(seq.name, len(seq.ikmers))
+        yield seq
+
+
 def alac(pstream, refrfile, ksize=31, delta=25, maxdiff=10000, match=1,
-         mismatch=2, gapopen=5, gapextend=0, greedy=False,
+         mismatch=2, gapopen=5, gapextend=0, greedy=False, min_ikmers=None,
          logstream=sys.stderr):
     assembler = assemble_greedy if greedy else assemble_fml_asm
-
     for partition in pstream:
-        contigs = [c for c in assembler(partition, logstream=logstream)]
+        reads = list(partition)
+        contigs = [c for c in assembler(reads, logstream=logstream)]
+        contigs = list(augment_and_mark(reads, contigs))
+        if min_ikmers is not None:
+            contigs = [c for c in contigs if len(c.ikmers) > min_ikmers]
+        if len(contigs) == 0:
+            continue
+
         targets = [t for t in localize(contigs, refrfile, ksize, delta=delta,
                                        logstream=logstream)]
         caller = call(
