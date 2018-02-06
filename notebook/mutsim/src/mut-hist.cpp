e50f0b653e62907f40cc478f0f1439aa4acf7f8d
Daniel Standage <daniel.standage@gmail.com>
2018-02-06 16:57:44 -0500

    Enable both counttables and seqfiles for k-mer input

--- notebook/mutsim/src/mut-hist.cpp
+++ notebook/mutsim/src/mut-hist.cpp
@@ -20,14 +20,18 @@ typedef struct
     double sampling_rate;
     int seed;
     std::string muttype;
+    ulong memory;
     std::string seqfile;
+    std::string refrfile;
     std::string refrcounts;
 } ProgramArgs;
 
 void print_usage(std::ostream& stream = std::cerr)
 {
-    stream << "Usage: snv-hist [options] genome.fa kmercounts.counttable\n";
+    stream << "Usage: mut-hist [options] genome.fa\n";
     stream << "  options:\n";
+    stream << "    -c    load genome k-mer counts from the specified counttable\n";
+    stream << "    -g    load genome k-mer counts from the specified seqfile\n";
     stream << "    -h    print this help message and exit\n";
     stream << "    -i    debug output interval (default: 1000000)\n";
     stream << "    -k    k-mer length (default: 31)\n";
@@ -36,15 +40,19 @@ void print_usage(std::ostream& stream = std::cerr)
     stream << "    -r    sampling rate (default: 1.0)\n";
     stream << "    -s    seed for random number generator (default: 42)\n";
     stream << "    -t    mutation type: snv (default), del\n";
+    stream << "    -y    memory consumption (in bytes; default: 2000000000)\n";
     stream << "    -z    deletion size (default: 5)\n";
 }
 
 void parse_args(int argc, const char **argv, ProgramArgs *args)
 {
     char c;
-    while ((c = getopt (argc, (char *const *)argv, "d:hi:k:l:m:r:s:t:y:z:")) != -1) {
-        if (c == 'd') {
-            args->delsize = atoi(optarg);
+    while ((c = getopt (argc, (char *const *)argv, "c:g:hi:k:l:m:r:s:t:y:z:")) != -1) {
+        if (c == 'c') {
+            args->refrcounts = optarg;
+        }
+        else if (c == 'g') {
+            args->refrfile = optarg;
         }
         else if (c == 'h') {
             print_usage(std::cout);
@@ -71,6 +79,9 @@ void parse_args(int argc, const char **argv, ProgramArgs *args)
         else if (c == 't') {
             args->muttype = optarg;
         }
+        else if (c == 'y') {
+            args->memory = strtoul(optarg, NULL, 10);
+        }
         else if (c == 'z') {
             args->delsize = atoi(optarg);
         }
@@ -82,7 +93,46 @@ void parse_args(int argc, const char **argv, ProgramArgs *args)
     }
 
     args->seqfile = argv[optind];
-    args->refrcounts = argv[optind + 1];
+    if (args->refrfile.compare("") == 0) {
+        args->refrfile = args->seqfile;
+    }
+}
+
+Counttable load_from_seqfile(std::string &seqfile, uint ksize, ulong memory)
+{
+    timepoint alloc_start = std::chrono::system_clock::now();
+    std::cerr << "# allocating counttable...";
+    std::vector<uint64_t> tablesizes = get_n_primes_near_x(4, memory / 4);
+    Counttable counttable(ksize, tablesizes);
+    timepoint alloc_end = std::chrono::system_clock::now();
+    std::chrono::duration<double> alloc_elapsed = alloc_end - alloc_start;
+    std::cerr << "done! (in " << alloc_elapsed.count() << " seconds)\n";
+
+    std::cerr << "# consuming k-mers from seqfile " << seqfile << "...";
+    timepoint consume_start = std::chrono::system_clock::now();
+    unsigned int seqs_consumed = 0;
+    unsigned long long kmers_consumed = 0;
+    counttable.consume_seqfile<FastxReader>(seqfile, seqs_consumed, kmers_consumed);
+    timepoint consume_end = std::chrono::system_clock::now();
+    std::chrono::duration<double> consume_elapsed = consume_end - consume_start;
+    std::cerr << "done! consumed " << seqs_consumed << " sequence(s) and "
+              << kmers_consumed << " " << ksize << "-mers (in "
+              << consume_elapsed.count() << " seconds)\n";
+
+    return counttable;
+}
+
+Counttable load_from_counttable(std::string &counttablefile)
+{
+    timepoint alloc_start = std::chrono::system_clock::now();
+    std::cerr << "# loading reference genome k-mers counts from " << counttablefile << "...";
+    std::vector<uint64_t> tablesizes = {1};
+    Counttable counttable(1, tablesizes);
+    counttable.load(counttablefile);
+    timepoint alloc_end = std::chrono::system_clock::now();
+    std::chrono::duration<double> alloc_elapsed = alloc_end - alloc_start;
+    std::cerr << "done! (in " << alloc_elapsed.count() << " seconds)\n";    
+    return counttable;
 }
 
 int main(int argc, const char **argv)
@@ -92,17 +142,12 @@ int main(int argc, const char **argv)
         return 0;
     }
 
-    ProgramArgs args = {5, 1000000, 31, 0, 16, 1.0, 42, "snv", "", ""};
+    ProgramArgs args = {5, 1000000, 31, 0, 16, 1.0, 42, "snv", 2000000000, "", "", ""};
     parse_args(argc, argv, &args);
 
-    timepoint alloc_start = std::chrono::system_clock::now();
-    std::cerr << "# loading reference genome k-mers counts from " << args.refrcounts << "...";
-    std::vector<uint64_t> tablesizes = {1};
-    Counttable counttable(1, tablesizes);
-    counttable.load(args.refrcounts);
-    timepoint alloc_end = std::chrono::system_clock::now();
-    std::chrono::duration<double> alloc_elapsed = alloc_end - alloc_start;
-    std::cerr << "done! (in " << alloc_elapsed.count() << " seconds)\n";
+    Counttable counttable = args.refrcounts.compare("") == 0 
+                              ? load_from_counttable(args.refrcounts)
+                              : load_from_seqfile(args.refrfile, args.ksize, args.memory);
 
     timepoint query_start = std::chrono::system_clock::now();
     std::cerr << "# iterating through " << args.seqfile << ", querying k-mer abundances...";
