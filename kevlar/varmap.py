41b6f4c713ea322c4611c0b75b28582547f6740e
Daniel Standage <daniel.standage@gmail.com>
2018-04-26 15:56:58 -0700

    Dedicated branch for SpuriousMismatch filter

--- kevlar/varmap.py
+++ kevlar/varmap.py
@@ -195,6 +195,9 @@ class VariantMapping(object):
             for call in chain(leftflankcaller, rightflankcaller):
                 if self.is_passenger(call):
                     call.filter(vf.PassengerVariant)
+                if abs(call.position - indel.position) > ksize and \
+                        int(call.attribute('IKMERS')) < ksize / 4:
+                    call.filter(vf.SpuriousMismatch)
                 yield call
         else:
             nocall = Variant(
