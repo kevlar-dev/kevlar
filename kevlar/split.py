d3863ac84f601fd83b3b2e84facb497730295a0b
Daniel Standage <daniel.standage@gmail.com>
2018-12-28 15:32:10 -0500

    Split workflow (#314)
    Accelerating the assemble/localize/align/call portion of the workflow via Python's threading module has proven unsuccessful, despite its dispatch of the assembly tasks the C extensions. This update modifies the `split`, `localize`, `call`, and `simlike` modules and the mark I Snakemake workflow to support parallel processing during that portion of the procedure.

--- kevlar/split.py
+++ kevlar/split.py
@@ -11,12 +11,22 @@ from itertools import cycle
 import kevlar
 
 
-def split(pstream, outstreams):
+def split(pstream, outstreams, maxreads=10000):
     """Split the partitions across the N outstreams."""
+    progress_indicator = kevlar.ProgressIndicator(
+        '[kevlar::split] processed {counter} partitions',
+        interval=100, breaks=[1000, 10000, 100000], usetimer=True,
+    )
     for partdata, outstream in zip(pstream, cycle(outstreams)):
         partid, partition = partdata
+        if len(partition) > maxreads:
+            message = 'WARNING: discarding partition '
+            message += 'with {} reads'.format(len(partition))
+            kevlar.plog('[kevlar::split]', message)
+            continue
         for read in partition:
             kevlar.print_augmented_fastx(read, outstream)
+        progress_indicator.update()
 
 
 def main(args):
@@ -25,7 +35,9 @@ def main(args):
     partstream = kevlar.parse_partitioned_reads(readstream)
     outstreams = list()
     for i in range(args.numfiles):
-        outfile = '{:s}.{:d}'.format(args.base, i + 1)
+        outfile = '{:s}.{:d}.augfastx'.format(args.base, i)
+        if args.infile.endswith('.gz'):
+            outfile += '.gz'
         os = kevlar.open(outfile, 'w')
         outstreams.append(os)
     split(partstream, outstreams)
